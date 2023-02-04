bc3de16ac253f8fa47a8957d08d81e720e82498a
Joneus Lê <lelacbinh3107@gmail.com>
2023-02-04 23:03:34 +0700

    Xây dựng UI phần Header #6

---
+++ src/Component/Header/guestNav/HeaderMenu/index.js
@@ -0,0 +1,167 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import ThemeBtn from '~/Component/ThemeBtn';
+// import HeaderSVG from '~/Component/Header/HeaderSVG';
+import TooltipWrapper from '~/Component/Header/TooltipWrapper';
+import styles from '~/Component/Header/guestNav/HeaderMenu/HeaderMenu.module.scss';
+
+const cx = classNames.bind(styles);
+
+const MENU_DATA = [
+    {
+        data: [
+            {
+                label: 'Tiếng Việt',
+                icon: {
+                    name: '#language_ico',
+                    alt: 'language',
+                },
+                children: {
+                    title: 'Ngôn ngữ',
+                    data: [
+                        {
+                            ID: 'vi',
+                            label: 'Tiếng Việt',
+                        },
+                        {
+                            ID: 'en',
+                            label: 'English',
+                            children: {
+                                title: 'English',
+                                data: [
+                                    {
+                                        ID: 'en-US',
+                                        label: 'America',
+                                    },
+                                    {
+                                        ID: 'en-UK',
+                                        label: 'England',
+                                    },
+                                ],
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                label: 'Phản hồi và trợ giúp',
+                icon: {
+                    name: '#help_ico',
+                    alt: 'help',
+                },
+                to: '/',
+            },
+            {
+                label: 'Phím tắt trên bàn phím',
+                icon: {
+                    name: '#shortcuts_ico',
+                    alt: 'shortcut',
+                },
+                to: false,
+            },
+            {
+                label: 'Chế độ tối',
+                icon: {
+                    name: '#theme_ico',
+                    alt: 'theme',
+                },
+                button: true,
+            },
+        ],
+    },
+];
+
+function HeaderMenu({ children, classNames }) {
+    const [display, setDisplay] = useState(MENU_DATA);
+
+    const handleChildren = (data) => {
+        if (!data) {
+            return;
+        }
+        setDisplay((prev) => [...prev, data]);
+    };
+
+    const handleBack = () => {
+        setDisplay((prev) => prev.slice(0, -1));
+    };
+
+    return (
+        <Tippy
+            visible
+            interactive
+            delay={[0, 800]}
+            placement="bottom-end"
+            render={(atts) => (
+                <div tabIndex="-1" {...atts}>
+                    <TooltipWrapper classNames={classNames}>
+                        <ul className={cx('c-headerMenu')}>
+                            {display[display.length - 1].title ? (
+                                <li className={cx('c-headerMenu__title')}>
+                                    <h3>
+                                        {display[display.length - 1].title}
+                                        <button onClick={handleBack}>
+                                            <svg alt="back">
+                                                <use xlinkHref="#back_ico"></use>
+                                            </svg>
+                                        </button>
+                                    </h3>
+                                </li>
+                            ) : (
+                                ''
+                            )}
+                            {display[display.length - 1].data.map(
+                                (data, index) => {
+                                    const WrapBox = ({ children }) => {
+                                        return data.to ? (
+                                            <Link to={data.to}>{children}</Link>
+                                        ) : (
+                                            <div
+                                                onClick={
+                                                    data.children
+                                                        ? () =>
+                                                              handleChildren(
+                                                                  data.children,
+                                                              )
+                                                        : undefined
+                                                }
+                                            >
+                                                {children}
+                                            </div>
+                                        );
+                                    };
+                                    return (
+                                        <li key={index}>
+                                            <WrapBox>
+                                                {data.icon ? (
+                                                    <svg alt={data.icon.alt}>
+                                                        <use
+                                                            xlinkHref={
+                                                                data.icon.name
+                                                            }
+                                                        ></use>
+                                                    </svg>
+                                                ) : (
+                                                    ''
+                                                )}
+                                                {data.label}
+                                            </WrapBox>
+                                            {data.button ? <ThemeBtn /> : ''}
+                                        </li>
+                                    );
+                                },
+                            )}
+                        </ul>
+                        {/* <HeaderSVG /> */}
+                    </TooltipWrapper>
+                </div>
+            )}
+        >
+            {children}
+        </Tippy>
+    );
+}
+
+export default HeaderMenu;
--- src/Component/Header/index.js
+++ src/Component/Header/index.js
@@ -9,7 +9,8 @@ import images from '~/assets/images';
 import SearchIco from '~/Component/Header/SearchIco';
 import TooltipWrapper from '~/Component/Header/TooltipWrapper';
 import HeaderSVG from '~/Component/Header/HeaderSVG';
-import HeaderMenu from '~/Layouts/Popper/HeaderMenu';
+import HeaderMenu from '~/Component/Header/guestNav/HeaderMenu';
+import GuestNav from './guestNav';
 
 const cx = classNames.bind(styles);
 const testData = [
@@ -148,47 +149,7 @@ function Header() {
                         </div>
                     </Tippy>
                 </div>
-                <div className={cx('l-header__navMenu')}>
-                    <button className={cx('c-btn01', 'c-btn01--white-bg')}>
-                        {/* Plus Icon */}
-                        <svg
-                            className={'l-header__plusIco'}
-                            width="1em"
-                            data-e2e=""
-                            height="1em"
-                            viewBox="0 0 16 16"
-                            fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                fillRule="evenodd"
-                                clipRule="evenodd"
-                                d="M8 2.5C7.58579 2.5 7.25 2.83579 7.25 3.25V7.25H3.25C2.83579 7.25 2.5 7.58579 2.5 8C2.5 8.41421 2.83579 8.75 3.25 8.75H7.25V12.75C7.25 13.1642 7.58579 13.5 8 13.5C8.41421 13.5 8.75 13.1642 8.75 12.75V8.75H12.75C13.1642 8.75 13.5 8.41421 13.5 8C13.5 7.58579 13.1642 7.25 12.75 7.25H8.75V3.25C8.75 2.83579 8.41421 2.5 8 2.5Z"
-                            ></path>
-                        </svg>
-                        Tải lên
-                    </button>
-                    <button className={cx('c-btn01')}>Đăng nhập</button>
-                    <span className={cx('l-header__more')}>
-                        <HeaderMenu classNames={cx('l-header__cornerMenu')}>
-                            <svg
-                                className={cx('l-header__moreIco')}
-                                width="1em"
-                                data-e2e=""
-                                height="1em"
-                                viewBox="0 0 48 48"
-                                fill="currentColor"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M24 4C26.2091 4 28 5.79086 28 8C28 10.2091 26.2091 12 24 12C21.7909 12 20 10.2091 20 8C20 5.79086 21.7909 4 24 4ZM24 20C26.2091 20 28 21.7909 28 24C28 26.2091 26.2091 28 24 28C21.7909 28 20 26.2091 20 24C20 21.7909 21.7909 20 24 20ZM24 36C26.2091 36 28 37.7909 28 40C28 42.2091 26.2091 44 24 44C21.7909 44 20 42.2091 20 40C20 37.7909 21.7909 36 24 36Z"
-                                ></path>
-                            </svg>
-                        </HeaderMenu>
-                    </span>
-                </div>
+                <GuestNav />
             </div>
             <HeaderSVG />
         </header>
